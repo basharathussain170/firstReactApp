031a982202efaac8bf27267c3839412a86c4bb0b
Basharat Hussain <basharathussain0170@gmail.com>
2025-07-24 21:49:44 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from './components/navigation'
+import HeroSection from './components/Hero'
+
+export default function App(){
+  return(
+    <div>
+     <Navigation/>
+     <HeroSection/>
+    </div>
+  )
+}
